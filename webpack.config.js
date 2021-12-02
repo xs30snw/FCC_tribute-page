8b61a489488fd6f59707c470961cf468d45cfcad
xs30snw <e4n22s@gmail.com>
2021-12-03 01:17:56 +0500

    Now Webpack generates project to /docs instead of /public

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
   
     output: {
-        path: path.resolve(__dirname, './public'),
+        path: path.resolve(__dirname, './docs'),
         clean: true,
     },
     
